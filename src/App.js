178e656f27dbcc83dd1e588b671afe4eb6c9b012
ezesali <eze.sali@hotmail.com>
2021-06-23 01:48:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,179 @@
+import React, {useEffect, useState} from 'react';
+import { TextField, Grid, Select } from '@material-ui/core';
+import IBMLogo from '../src/ibm-logo.webp';
+import DragDrop from '../src/components/Drag&Drop';
+import makeStyles  from '@material-ui/styles/makeStyles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import { DialogContent, DialogActions, DialogTitle } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { isMobile } from 'react-device-detect';
+import sadFace from '../src/sadUnsupported.png';
+
+const useStyles = makeStyles({
+  SubmitButton: {
+    color: 'white', 
+    textDecoration: 'none', 
+    backgroundColor: '#5360D1', 
+    width:'10pc',
+    padding: '10px', 
+    borderRadius: '50px', 
+    display: 'inline-block',
+    cursor:'pointer'
+  },
+});
+
+export default function App(props) {
+
+  const styles = useStyles();
+
+  //const [files, setfiles] = useState([]);
+  const [disableSubmit, setDisableSubmit] = useState(true);
+  const [errorInputName, seterrorInputName] = useState('');
+  const [errorInputCRQ, seterrorInputCRQ] = useState('');
+  const [errorInputFrente, seterrorInputFrente] = useState('');
+  const [Name, setName] = useState('');
+  const [CRQ, setCRQ] = useState('');
+  const [Frente, setFrente] = useState('');
+  const [myFiles, setmyFiles] = useState({});
+  const [openAlert, setOpenAlert] = useState(false);
+  const [MsgFile, setMsgFile] = useState('');
+  
+  useEffect(() => {
+    //files.forEach((file) => URL.revokeObjectURL(file.preview)); 
+    if (Name.length > 0 && CRQ.length > 0 && Frente.length > 0 && myFiles.length > 0){
+      console.log('Cambia')
+      setDisableSubmit(false);
+    }
+    else{
+      setDisableSubmit(true);
+    }
+  }, [Name,CRQ,Frente,myFiles]);
+
+
+  /*function addFile(file) {
+    console.log(file);
+    setfiles({
+      files: file.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })
+      )
+    });
+  };*/
+
+  function handleError(e) {
+    console.log(e.target.name)
+    console.log(e.target.value)
+
+    if (e.target.value === '' && e.target.required){
+      if (e.target.name === 'Nombre'){
+        seterrorInputName('El '+ e.target.name + ' es requerido para crear la paquetizacion')
+      }
+      if (e.target.name === 'CRQ'){
+        seterrorInputCRQ('El '+ e.target.name + ' es requerido para crear la paquetizacion')
+      }
+      if (e.target.name === 'Frente'){
+        seterrorInputFrente('El '+ e.target.name + ' es requerido para crear la paquetizacion')
+      }
+    }else{
+
+      seterrorInputName('')
+      seterrorInputCRQ('')
+      seterrorInputFrente('')
+      
+    }
+  }
+
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+
+  function showAlert(){
+    return (
+      <Dialog onClose={handleCloseAlert} open={openAlert}>
+        <DialogTitle onClose={handleCloseAlert}>
+          Error
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            El archivo "{MsgFile}" no se puede agregar ya que se agrego anteriormente
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCloseAlert} color="primary">
+            Entendido
+          </Button>
+        </DialogActions>
+      </Dialog>
+    )
+
+  }
+
+  return (
+    <div  style={{display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor:'grey' }}>
+      {isMobile ? 
+      <Grid container direction="column" item xl={4} align="center" style={{height:'100vh', backgroundColor:'white', overflowY:'scroll' }}>
+        <div style={{alignItems: 'center', justifyContent: 'center'}}>
+          <img style={{padding:'30px'}} width="200" height="100" src={IBMLogo} alt="Logo" />
+          <h3>HERRAMIENTA PARA<br/>LA PAQUETIZACION DE ENTREGAS DE PASAJE DE AMBIENTES</h3>
+          <img style={{padding:'30px'}} width="200" height="200" src={sadFace} alt="Unsupported Device" />
+          <h4 style={{fontFamily:'monospace', fontStyle:'oblique'}}>
+            EL DISPOSITIVO EN EL QUE INTENTA ABRIR LA HERRAMIENTA NO ES COMPATIBLE.<br/>
+            POR FAVOR ABRA LA WEB DESDE UN ORDENADOR.
+          </h4>
+        </div>
+      </Grid>
+      :
+      <Grid container direction="column" item xs={10} align="center" style={{height:'100vh', backgroundColor:'white', overflowY:'scroll' }}>
+        <div style={{alignItems: 'center', justifyContent: 'center'}}>
+          <img style={{padding:'30px'}} width="200" height="100" src={IBMLogo} alt="Logo" />
+          <h3>HERRAMIENTA PARA LA PAQUETIZACION<br/>DE ENTREGAS DE PASAJE DE AMBIENTES</h3>
+          <div style={{width:'63%',border:'dashed',padding:'20px 0 20px 0',fontFamily:'monospace', fontSize:'13px'}}>  
+            <h4>1- Ingresa el Nombre y/o Descripcion de la paquetizacion para dejarlo en<br/>los comentarios del archivo .drv</h4>
+            <h4>2- Ingresa el CRQ correspondiente, esto sirve para ponerle el nombre a los<br/>Archivos Zipeados y tambien colocar en el archivo .drv</h4>
+            <h4>3- Elegi el Frente en el cual estuviste trabajando, tambien se va a colocar como<br/>formato del archivo zipeado y .drv</h4>
+            <h4>4- Carga los archivos de los Procedimientos/Scripts necesarios que tenes que entregar.</h4>
+            <h4>5- Corrobora que todo este correcto y presiona Generar.<br/><br/>Listo! Esto te va a abrir una ventana donde vas a poder guardar el .zip con la entrega generada.</h4>
+          </div>
+          <form style={{paddingTop:'40px', display:'-ms-grid'}} noValidate onSubmit={() => console.log('SUBMIT FORM')} autoComplete="off">
+            <div style={{padding:'20px 0 20px 0'}}>
+              <TextField onChange={(e) => setName(e.target.value) } onFocus={() => seterrorInputName('')} name='Nombre' onBlur={ (e) => handleError(e)} autoFocus={true} style={{width:'60%'}} placeholder='CRQXXXXXXXXXX - Historia PRJ-XXX - CRM (IBM)' required label="Nombre/Descripcion del Aplica"/>
+              {errorInputName ? <h5 style={{color:'red', margin:'0'}}>{errorInputName}</h5> : ''}
+            </div>
+            <div style={{padding:'20px 0 20px 0'}}>
+              <TextField onChange={(e) => setCRQ(e.target.value)} onFocus={() => seterrorInputCRQ('')} name='CRQ' onBlur={ (e) => handleError(e)} style={{width:'60%'}} required label="CRQ"/>
+              {errorInputCRQ ? <h5 style={{color:'red', margin:'0'}}>{errorInputCRQ}</h5> : ''}
+            </div>
+            <div style={{padding:'20px 0 20px 0'}}>
+              <Select onChange={(e) => setFrente(e.target.value)} onFocus={() => seterrorInputFrente('')} 
+                      native
+                      required
+                      name='Frente'
+                      style={{width:'60%'}}
+                      defaultValue=''
+                      onBlur={ (e) => handleError(e)}>
+                <option label="Seleccione un valor" value='' />
+                <option value={'CRM'}>CRM</option>
+                <option value={'Billing'}>Billing</option>
+                <option value={'Cobranzas'}>Cobranzas</option>
+                <option value={'Corporativos'}>Corporativos</option>
+                <option value={'Promociones Billing'}>Promociones Billing</option>
+                <option value={'Inv. de Red'}>Inv. de Red</option>
+                <option value={'Promociones Coporativos'}>Promociones Coporativos</option>
+                <option value={'Ordenes'}>Ordenes</option>
+              </Select>
+              {errorInputFrente ? <h5 style={{color:'red', margin:'0'}}>{errorInputFrente}</h5> : ''}
+            </div>
+            <DragDrop attachFiles={filesArray => setmyFiles(filesArray)} 
+            showAlert={(open, file) => { setOpenAlert(open); setMsgFile(file)}}/>
+            <Button className={styles.SubmitButton}
+                    type="submit" value="Submit"
+                    disabled={disableSubmit}>Generar</Button>
+          </form>
+          {showAlert()}
+        </div>
+      </Grid>}
+    </div>
+  );
+}
---
+++ src/components/Drag&Drop.js
@@ -0,0 +1,110 @@
+import React, {useMemo, useState, useCallback, useEffect} from 'react';
+import {useDropzone} from 'react-dropzone';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+
+const baseStyle = {
+  flex: 1,
+  width:'50%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: 'black',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  cursor:'pointer',
+  transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
+
+export default function DragDrop(props) {
+
+  const [myFiles, setMyFiles] = useState([])
+
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach((file) => {
+      myFiles.forEach(function (myfile) {
+        if (myfile.path === file.path){
+          myFiles.splice(myFiles.indexOf(myfile), 1)
+          console.log('No se agrego archivo')
+          console.log(props)
+          props.showAlert(true, myfile.path)
+        }
+      });
+    })
+    setMyFiles([...myFiles, ...acceptedFiles])
+  }, [myFiles, props])
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+  });
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {}),
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
+
+  const removeFile = file => () => {
+    const newFiles = [...myFiles]
+    newFiles.splice(newFiles.indexOf(file), 1)
+    setMyFiles(newFiles)
+  }
+
+  useEffect(() => {
+
+    props.attachFiles(myFiles)
+
+  }, [myFiles,props])
+
+  const files = myFiles.map((file,index) => (
+    <div key={index} style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
+        <div style={{marginRight: '20px'}}>
+            - {file.path}
+        </div>
+        <IconButton id={file.path} onClick={removeFile(file)} size='small'>
+            <DeleteOutlineIcon style={{color:'red'}}/>
+        </IconButton>
+    </div>
+  ))
+
+  return (
+    <div style={{alignItems:'center'}} className="container">
+        <div {...getRootProps({style})}>
+            <input required {...getInputProps()} />
+            {myFiles.length > 0 ? <h1>{myFiles.length} Archivos subidos</h1>: <p>Agrega los procedimientos y/o scripts que queres entregar en el aplica arrastrandolos o haciendo click en el cuadro</p>}
+        </div>
+        <aside style={{paddingBottom:'25px'}}>
+            {myFiles.length > 0 ? <h3>Files</h3> : ''}
+            {files}
+        </aside>
+    </div>
+  );
+}
